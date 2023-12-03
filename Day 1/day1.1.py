ce956f40753c8ed288db7f2b337c9b3935053317
Jonna <jonna.van.doorn@hotmail.com>
2023-12-03 17:57:55 +0100

    Day1.1

---
+++ Day 1/day1.1.py
@@ -0,0 +1,27 @@
+# Open data
+with open('data1.1.txt', 'r') as f:
+    lines = f.read().split('\n')
+f.close()
+
+lines.pop()
+
+# Prepare for-loop
+digits = ['0','1','2','3','4','5','6','7','8','9']
+number = ''
+total = 0
+
+# Get digits and add them together
+for line in lines:
+    for char in line:
+        if char in digits:
+            number = char
+            break
+    for char in reversed(line):
+        if char in digits:
+            number += char
+            break
+    total += int(number)
+
+print(total)
+
+
